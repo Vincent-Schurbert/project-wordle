0b4c534b1cf2e64dd99ad72c4d1cc9bb6d42f913
Vincent Schurbert <schurbert@landaumedia.de>
2025-03-20 16:59:39 +0100

    exercise 4

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -4,6 +4,7 @@ import { sample } from "../../utils";
 import { WORDS } from "../../data";
 import GuessWord from "../GuessWord/GuessWord";
 import GuessList from "../GuessList/GuessList";
+import { checkGuess } from "../../game-helpers";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -14,7 +15,9 @@ function Game() {
   const [guessList, setGuessList] = React.useState([]);
 
   function addGuessToList(guessedWord) {
-    const newGuessList = [...guessList, guessedWord];
+    const newGuess = checkGuess(guessedWord, answer);
+
+    const newGuessList = [...guessList, newGuess];
     setGuessList(newGuessList);
   }
 
--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -2,12 +2,11 @@ import React from "react";
 import { range } from "../../utils";
 
 function Guess({ word }) {
-  console.log(word);
   return (
     <p className="guess">
       {range(5).map((num) => (
-        <span className="cell" key={num}>
-          {word ? word[num] : ""}
+        <span className={`cell ${word ? word[num].status : null}`} key={num}>
+          {word ? word[num].letter : ""}
         </span>
       ))}
     </p>
