b474e2df9e5fb854fa42838d0faec7e98c7f8c6e
Vincent Schurbert <schurbert@landaumedia.de>
2025-03-21 15:25:26 +0100

    added keyboard

---
+++ src/components/Keyboard/Keyboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Keyboard() {
+  const rows = [
+    ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
+    ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
+    ["Z", "X", "C", "V", "B", "N", "M"],
+  ];
+  return <div></div>;
+}
+
+export default Keyboard;
